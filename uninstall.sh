b82bf4c78bade21bd882127c9fbd4131e4088b42
balazs <t.balazs1986@gmail.com>
2019-02-12 13:41:04 +0100

    Change install directory.

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # remove includes
-sudo rm -r -f /usr/local/include/prolog
+sudo rm -r -f ~/local/include/prolog
 # remove library files
-sudo rm -r -f /usr/local/lib/prolog
+sudo rm -r -f ~/local/lib/prolog
